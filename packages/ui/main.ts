ed744a497e3e8994b75f2105ab7556f0bb958bcc
comfuture <comfuture@iplateia.com>
2022-04-03 14:50:09 +0000

    some rough implementaions

---
+++ packages/ui/main.ts
@@ -0,0 +1,7 @@
+import "./base.css";
+import "./src/theme/default.css";
+import "./src/button.css";
+import "./src/dialog.css";
+import "./src/group.css";
+import "./src/form.css";
+import "./src/input.css";
