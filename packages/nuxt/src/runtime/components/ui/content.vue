89d4218f6d211bf9bbb8c07f2f2f2944205ffe16
comfuture <comfuture@iplateia.com>
2024-10-21 06:06:37 +0000

    refactor: optimize layout and styles in content component
    experimental uses astrics for flex width

--- packages/nuxt/src/runtime/components/ui/content.vue
+++ packages/nuxt/src/runtime/components/ui/content.vue
@@ -1,7 +1,7 @@
 <script lang="tsx">
 import { inject, h } from 'vue'
-import { useSlots, useAttrs, useId, resolveComponent, defineComponent, type PropType } from '#imports'
 import type { TabDefinition } from './tabs.vue'
+import { useSlots, useAttrs, useId, resolveComponent, defineComponent, type PropType } from '#imports'
 
 export default defineComponent({
   props: {
@@ -33,12 +33,12 @@ export default defineComponent({
         el.setAttribute('aria-expanded', el.open ? 'true' : 'false')
       }
       return () => (
-        <details id={ id } class="ui" name={ name } open={ props.active } { ...attrs } aria-expanded={ props.active ? 'true' : 'false' } onToggle={ handleToggle }>
-          <summary aria-controls={ id }>
-            { slots.header?.() ?? props.title }
+        <details id={id} class="ui" name={name} open={props.active} {...attrs} aria-expanded={props.active ? 'true' : 'false'} onToggle={handleToggle}>
+          <summary aria-controls={id}>
+            {slots.header?.() ?? props.title}
           </summary>
           <div class="content" >
-            { slots.default?.() }
+            {slots.default?.()}
           </div>
         </details>
       )
@@ -52,18 +52,18 @@ export default defineComponent({
       // render tab content
       // return () => h('section', { id, class: ['content', { active: props.active }] }, slots.default?.())
       return () => (
-        <section role="tabpanel" aria-label={ props.title } id={ id } class={ ['content', { active: props.active }] } { ...props } { ...attrs }>
-          { container }
-          { slots.default?.() }
+        <section role="tabpanel" aria-label={props.title} id={id} class={['content', { active: props.active }]} {...props} {...attrs}>
+          {container}
+          {slots.default?.()}
         </section>
-      );
+      )
     } else {
       // render normal content
       const Tag = attrs.is ? resolveComponent(attrs.is as string) : 'section'
       const hasHeader = slots.header || props.title
       return () => h(Tag, { ...props, ...attrs, class: ['ui content'] }, [
         hasHeader && h('header', {}, slots.header?.() ?? props.title),
-        slots.default?.()
+        slots.default?.(),
       ])
       // XXX: type inference fails here
       // return () => (
@@ -88,16 +88,37 @@ export default defineComponent({
       gap: 0.5rem;
     }
 
-    &.columns {
+    &.columns,
+    &.wrap {
       display: flex;
       flex-direction: row;
       gap: 0.5rem;
+    }
+
+    &.wrap {
+      flex-wrap: wrap;
     }
 
     &.responsible {
       display: flex;
       flex-direction: column;
       gap: 0.5rem;
+
+      >.\* {
+        flex: 1;
+      }
+
+      >.\*\* {
+        flex: 2;
+      }
+
+      >.\*\*\* {
+        flex: 3;
+      }
+
+      >.\*\*\*\* {
+        flex: 4;
+      }
 
       @media screen and (min-width: 768px) {
         flex-direction: row;
