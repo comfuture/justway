6b2b8f22e695206927829bd23c2376b09dc7ac07
comfuture <comfuture@iplateia.com>
2024-10-25 07:41:06 +0000

    Refactor breadcrumb component and add @justway/ui package

--- packages/nuxt/src/runtime/components/ui/breadcrumb.vue
+++ packages/nuxt/src/runtime/components/ui/breadcrumb.vue
@@ -1,38 +1,43 @@
+<script setup lang="ts" generic="T extends Breadcrumb[]">
+import "@justway/ui/style/breadcrumb.css";
+import { h } from 'vue'
+
+export type BreadcrumbItem = {
+  label: string
+  href?: string
+} | string
+
+const props = withDefaults(defineProps<{
+  items?: T
+}>(), {
+  items: []
+})
+
+const renderItem = (def: BreadcrumbItem) => {
+  const isString = (val: unknown): val is string => typeof val === 'string'
+  const isObject = (val: unknown): val is BreadcrumbItem => typeof val === 'object'
+
+  if (isString(def)) {
+    return h('li', { 'aria-current': 'page' }, def)
+    // return <li aria - current="page" > { def } </li>
+  }
+
+  if (isObject(def)) {
+    return h('li', {}, h('a', { href: def.href }, def.label))
+    // return <li><a href={ def.href }> { def.label } < /a></li >
+  }
+  return null
+}
+</script>
+
 <template>
   <nav class="ui breadcrumb" aria-label="Breadcrumb">
     <ol>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li aria-current="page">Current Page</li>
+      <slot>
+        <template v-for="item in props.items">
+          {{ renderItem(item) }}
+        </template>
+      </slot>
     </ol>
   </nav>
 </template>
-<style>
-nav.ui.breadcrumb {
-  padding: 0.5rem 0;
-
-  ol {
-    margin: 0;
-    padding-left: 0;
-    list-style: none;
-
-    li {
-      display: inline;
-
-      &+li::before {
-        display: inline-block;
-        margin: 0 0.5em;
-        transform: rotate(15deg);
-        border-right: 0.1em solid currentcolor;
-        height: 0.8em;
-        content: "";
-      }
-    }
-  }
-
-  [aria-current="page"] {
-    font-weight: 700;
-    text-decoration: none;
-  }
-}
-</style>
