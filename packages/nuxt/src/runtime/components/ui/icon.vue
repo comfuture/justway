2bc45172bc7e7ca145dd09b427c8533ed17c8d69
comfuture <comfuture@iplateia.com>
2024-10-25 07:43:44 +0000

    Refactor icon component prevent reflow on rendering ligature (and add font loading logic)

--- packages/nuxt/src/runtime/components/ui/icon.vue
+++ packages/nuxt/src/runtime/components/ui/icon.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   name: string;
   filled?: boolean;
 }>();
+
+// const isFontLoaded = ref(document?.fonts?.check('1em Material Symbols Outlined'));
+// document?.fonts?.addEventListener('loadingdone', () => {
+//   isFontLoaded.value = true;
+// });
+// const fontText = computed(() => (isFontLoaded.value ? props.name : ' '));
 </script>
 <template>
   <i class="material-symbols-outlined" :class="{ filled }">
@@ -13,6 +19,8 @@ defineProps<{
 @import url(https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200);
 
 .material-symbols-outlined {
+  width: 1em;
+  overflow: hidden;
   vertical-align: middle;
   font-variation-settings:
     'FILL' 0,
